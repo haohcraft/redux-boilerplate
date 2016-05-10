ac23173e89b8b64bbec98c0d37d81f321bfc5f97
Hao Huang <hhuang@dataminr.com>
2016-05-09 23:32:24 -0400

    setup eslint

---
+++ js/pages/home.js
@@ -0,0 +1,7 @@
+import React, {PropTypes, Component} from 'react';
+export default class HomePage extends Component {
+    static propTypes = {};
+    render() {
+        return <div>This Home Page</div>;
+    }
+}
