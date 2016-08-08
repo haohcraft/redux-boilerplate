d402370e0ad84fcc0fa7354fff8a2ff765d7cfc3
Hao Huang <hhuang@dataminr.com>
2016-08-08 10:03:28 -0400

    bring in form

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -3,30 +3,26 @@ import Header from 'components/header';
 import Footer from 'components/footer';
 import Info from 'components/info';
 
-// import Form from 'containers/form';
-// import Finish from 'containers/finishBtn';
+import Form from 'containers/form';
+import Finish from 'containers/finishBtn';
 import PageContainer from './container';
-import Confirm from './confirm';
 
 export default class HomePage extends Component {
     static propTypes = {
         title: PropTypes.string
     };
     render() {
-        // const FormView = Form.View;
+        const FormView = Form.View;
         return <div ref="test">
             <Header name="Café Reserve" />
             <Info />
             <PageContainer>
-                <Confirm />
+                <div>
+                    <FormView />
+                    <Finish />
+                </div>
             </PageContainer>
             <Footer />
         </div>;
     }
 }
-
-
-// <div>
-//     <FormView />
-//     <Finish />
-// </div>
